126f70ab0d49cb366ca7a5458cc8e66cec4bba36
Josphia <josphiagladlin@gmail.com>
2026-06-16 16:45:54 +0530

    2

--- test.py
+++ test.py
@@ -1,16 +1,28 @@
-s = input()
+# s = input()
 
-res = 0 
+# res = 0 
 
-for i in range(len(s)):
-    toCheck = ""
-    for j in range(i, len(s)):
-        if s[j] not in toCheck:
-            toCheck+= s[j]
-        else:
-            break
+# for i in range(len(s)):
+#     toCheck = ""
+#     for j in range(i, len(s)):
+#         if s[j] not in toCheck:
+#             toCheck+= s[j]
+#         else:
+#             break
 
-    if len(toCheck)>res:
-        res = len(toCheck)
+#     if len(toCheck)>res:
+#         res = len(toCheck)
 
-print(res)
+# print(res)
+
+
+arr = list(input())#[1,2,3,6,4]#input()
+res = ""
+
+if len(arr) == 1:
+    res = "0"
+else:
+    for i in range(1, len(arr)-1):
+        if arr[i] > arr[i-1] and arr[i] > arr[i+1]:
+            res += str(i)
+print(res[-1])
