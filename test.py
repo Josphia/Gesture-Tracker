5b5d9254d9e7cd307f292c7ab532e32316e57402
Josphia <josphiagladlin@gmail.com>
2026-06-18 00:36:52 +0530

    2

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
 s = "babad"
 
-for i in range(0, len(s)):
-    toCheck = ""
-    for j in range(i, len(s)):
-        toCheck += s[j]
-        print(toCheck)
-        # while(len(toCheck)>1):
-        #     if toCheck == toCheck[:-1]:
-        #         print(toCheck)
-        #         break
