04cd0ef8476c4ed0f087fe1ea34c4c9d3ee476ec
Josphia <josphiagladlin@gmail.com>
2026-06-18 00:17:16 +0530

    2

--- test.py
+++ test.py
@@ -1,33 +1,11 @@
-# s = input()
-
-# res = 0 
-
-# for i in range(len(s)):
-#     toCheck = ""
-#     for j in range(i, len(s)):
-#         if s[j] not in toCheck:
-#             toCheck+= s[j]
-#         else:
-#             break
-
-#     if len(toCheck)>res:
-#         res = len(toCheck)
-
-# print(res)
-
-
-s = "abcabcbb"#input()
-
-count = 0 
+s = "babad"
 
 for i in range(0, len(s)):
     toCheck = ""
     for j in range(i, len(s)):
-        if s[j] not in toCheck:
-            toCheck += s[j]
-        else:
-            break
-        if len(toCheck)>count:
-            count = len(toCheck)
-    
-print(count)
+        toCheck += s[j]
+        print(toCheck)
+        # while(len(toCheck)>1):
+        #     if toCheck == toCheck[:-1]:
+        #         print(toCheck)
+        #         break
